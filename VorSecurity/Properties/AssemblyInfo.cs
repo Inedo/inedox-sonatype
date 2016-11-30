cdfd3da11cd973381d488d5e77278f9fd201468e
Karl Harnagy <kharnagy@inedo.com>
2016-11-30 13:51:25 -0500

    Initial commit.

---
+++ VorSecurity/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Vor Security")]
+[assembly: AssemblyDescription("Contains a Vulnerability Source which connects to Vor Security.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Inedo, LLC")]
+[assembly: AssemblyProduct("ProGet")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
