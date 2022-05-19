609d7b1f7ee634ea9b09af06dd7058f5acebb76b
Richard Hessinger <rhessinger@inedo.com>
2022-05-19 16:31:12 -0400

    Updated Sonatype extension to .net 6.0 and Inedo.SDK 2.0.1
    closes #15

--- Sonatype/InedoExtension/Properties/AssemblyInfo.cs
+++ Sonatype/InedoExtension/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using Inedo.Extensibility;
 [assembly: AssemblyDescription("Contains a Vulnerability Source which connects to Sonatype's OSS Index. Previously, this was the Vor Security Extension.")]
 [assembly: AssemblyCompany("Inedo, LLC")]
 [assembly: AssemblyProduct("ProGet")]
-[assembly: AssemblyCopyright("Copyright © Inedo 2018")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyCopyright("Copyright © Inedo 2022")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
 [assembly: ComVisible(false)]
 
 [assembly: AppliesTo(InedoProduct.ProGet)]
